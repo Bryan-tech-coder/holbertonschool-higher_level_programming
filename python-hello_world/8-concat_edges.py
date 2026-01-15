7064d11a6e861f0926119a11f27a89e864a95a5b
Bryan Ramos <br1173626@gmail.com>
2026-01-14 22:19:40 -0400

    c

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print(str[39:67] + str[107:112] + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = "object-oriented programming with Python"
+print(str)
