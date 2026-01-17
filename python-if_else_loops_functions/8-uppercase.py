c5f36116b7983977dbb1ee590d7c9e41777071eb
Bryan Ramos <br1173626@gmail.com>
2026-01-17 17:18:19 -0400

    up

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):  
+    chaine = ""
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            chaine += chr(ord(str[i]) - 32)
+        else:
+            chaine += str[i]
+    print("{}".format(chaine))
