33322e08dde4b20bfd3d9b8e4467e427bc02c57a
Bryan Ramos <br1173626@gmail.com>
2026-01-21 12:12:09 -0400

    max i

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(list=[]):  
+    if len(list) == 0:
+        return None
+    max_value = list[0]
+    for num in list:
+        if num > max_value:
+            max_value = num
+    return max_value
