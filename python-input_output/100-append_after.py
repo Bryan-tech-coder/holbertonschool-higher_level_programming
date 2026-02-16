4859836ce2055c6f2e6fe00a9419edccd7b584af
Bryan Ramos <br1173626@gmail.com>
2026-02-16 01:47:48 -0400

    .

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Module that defines a function to insert a line after matching lines in a file."""
+"""Module that defines a function to insert
+a line after matching lines in a file."""
 
 
-def append_after(filename="", search_string="", new_string=""):
+def append_after(filename="", search_string="",
+                 new_string=""):
     """Inserts new_string into filename after each line containing search_string."""
     with open(filename, "r", encoding="utf-8") as f:
         lines = f.readlines()
