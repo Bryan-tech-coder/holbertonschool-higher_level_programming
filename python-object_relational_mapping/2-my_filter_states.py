f497513d55d53aea02155b24f7c7979a756c23c1
Bryan Ramos <br1173626@gmail.com>
2026-03-08 14:29:39 -0400

    .

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,8 @@ Usage: ./2-my_filter_states.py <username> <password> <database> <state_name>
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    # Get arguments
+def main():
+    """Connects to MySQL and prints all states matching the given name."""
     username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
     # Connect to MySQL
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    # Use format to create query
+    # Query with format as requested
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
     cur.execute(query)
 
@@ -32,3 +32,6 @@ if __name__ == "__main__":
 
     cur.close()
     db.close()
+
+if __name__ == "__main__":
+    main()
